c2b03e076f2f05a13f49e10d71bd2fcb33132965
Andrés Rivas <andreselias2303@gmail.com>
2021-11-22 15:02:03 -0300

    Auth with jwt

--- src/main/kotlin/com/ae/petapi/config/security/JWTUtil.kt
+++ src/main/kotlin/com/ae/petapi/config/security/JWTUtil.kt
@@ -1,5 +1,6 @@
 package com.ae.petapi.config.security
 
+import io.jsonwebtoken.Claims
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.SignatureAlgorithm
 import org.springframework.security.core.userdetails.UserDetails
@@ -13,3 +14,14 @@ fun generateToken(userDetail: UserDetails): String = Jwts.builder()
     .setExpiration(Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
     .signWith(SignatureAlgorithm.HS256, KEY)
     .compact()
+
+fun validateToken(token: String, userDetail: UserDetails): Boolean {
+    return userDetail.username.equals(extractUsername(token)) && !isTokenExpired(token)
+}
+
+fun isTokenExpired(token: String): Boolean = getClaims(token).expiration.before(Date())
+
+fun extractUsername(token: String): String = getClaims(token).subject
+
+private fun getClaims(token: String): Claims = Jwts.parser().setSigningKey(KEY).parseClaimsJws(token).body
+
--- src/main/kotlin/com/ae/petapi/config/security/SecurityConfig.kt
+++ src/main/kotlin/com/ae/petapi/config/security/SecurityConfig.kt
@@ -1,5 +1,6 @@
 package com.ae.petapi.config.security
 
+import com.ae.petapi.config.security.filter.JWTFilterRequest
 import com.ae.petapi.feature.auth.service.UserDetailService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
@@ -8,10 +9,13 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @EnableWebSecurity
 class SecurityConfig(
-    @Autowired private val userDetailService: UserDetailService
+    @Autowired private val userDetailService: UserDetailService,
+    @Autowired private val jWTFilterRequest: JWTFilterRequest
 ) : WebSecurityConfigurerAdapter() {
 
     override fun configure(auth: AuthenticationManagerBuilder?) {
@@ -21,6 +25,9 @@ class SecurityConfig(
     override fun configure(http: HttpSecurity?) {
         http?.csrf()?.disable()?.authorizeRequests()?.antMatchers("/**/authenticate")?.permitAll()
             ?.anyRequest()?.authenticated()
+            ?.and()?.sessionManagement()?.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+        http?.addFilterBefore(jWTFilterRequest, UsernamePasswordAuthenticationFilter::class.java)
     }
 
     @Bean
